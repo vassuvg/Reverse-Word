0a72548ac816804a8b66b57aef0c6945d3df9ff2
vassuvg <gagnejav@sheridancollege.ca>
2022-05-26 14:11:07 -0400

    version2

---
+++ src/starpyramid/StarPyramid.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package starpyramid;
+
+/**
+ *
+ * @author Vassu
+ */
+import java.util.*;
+public class StarPyramid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int a=5;
+        int b=4;
+        int c=3;
+        for(int i=0;i<a;i++){
+            System.out.print("*");
+        System.out.println();
+        }
+        for(int i=0;i<b;i++){
+            System.out.print("*");
+        System.out.println();
+        }
+        for(int i=0;i<c;i++){
+            System.out.print("*");
+        System.out.println();
+        }
+        
+                    
+    }
+        
+                
+}
+    
